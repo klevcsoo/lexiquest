75eb761905183fb912346f2df7d2bfd2f601910a
Keve Lajsz <kevelajsz@gmail.com>
2023-09-11 22:35:09 +0200

    implement basic menu layout

--- lexiquest-client/src/layouts/MenuLayout.tsx
+++ lexiquest-client/src/layouts/MenuLayout.tsx
@@ -1,5 +1,16 @@
+import {Button} from "../components/Button";
+import {Link} from "react-router-dom";
+
 export function MenuLayout() {
     return (
-        <div>menü</div>
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+        flex flex-col items-center justify-center gap-4">
+            <h1 className="font-thin text-6xl text-center mb-12 select-none">
+                LexiQuest
+            </h1>
+            <Link to="/game" className="w-full">
+                <Button text="Játék indítása"/>
+            </Link>
+        </div>
     );
 }
