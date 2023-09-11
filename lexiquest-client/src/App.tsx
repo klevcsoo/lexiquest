43810890cc762bb331958650f884b8ec43c952a5
Keve Lajsz <kevelajsz@gmail.com>
2023-09-11 21:16:52 +0200

    add Tailwind CSS to React project

--- lexiquest-client/src/App.tsx
+++ lexiquest-client/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
     return (
-        <div>
+        <div className="text-center text-xl font-bold">
             Hello, World!
         </div>
     );
