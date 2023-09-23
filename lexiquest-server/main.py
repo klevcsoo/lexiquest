c04e151dac8fc696802bffe1e7bfa0a64588b9da
Keve Lajsz <kevelajsz@gmail.com>
2023-09-23 12:49:02 +0200

    add API CORS

--- lexiquest-server/main.py
+++ lexiquest-server/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import models
 from database import engine, SessinLocal
 from routes.word import word
@@ -9,4 +10,12 @@ app = FastAPI()
 app.include_router(word)
 app.include_router(user)
 app.include_router(validate)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+
+)
 models.Base.metadata.create_all(bind = engine)
