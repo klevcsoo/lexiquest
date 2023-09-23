e582cb11dcc47a50129f8ec4722b2b6052b7646d
FetterDavid <fetter.david00@gmail.com>
2023-09-23 22:59:59 +0200

    validation fix

--- lexiquest-server/utilities/guess_validator.py
+++ lexiquest-server/utilities/guess_validator.py
@@ -1,12 +1,14 @@
 def get_validation_result(str1, str2):
     eredmeny = []
+    i=0
     for char1 in str1:
         if char1 in str2:
-            if str1.index(char1) == str2.index(char1):
+            if str1[i] == str2[i]:
                 eredmeny.append("1")
             else:
                 eredmeny.append("0")
         else:
             eredmeny.append("-1")
+        i = i+1
     
     return ";".join(eredmeny)
