c27636295b7f1974f823f87d609838f4a1473ce3
FetterDavid <fetter.david00@gmail.com>
2023-09-23 10:50:50 +0200

    validate user and attempt check

--- lexiquest-server/routes/word.py
+++ lexiquest-server/routes/word.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, status, APIRouter
+from fastapi import Depends, status, APIRouter, HTTPException
 from database import engine, SessinLocal
 from typing import Annotated
 import models
@@ -6,6 +6,7 @@ from schema.wordbase import WordBase
 from sqlalchemy.orm import Session
 from utilities.dictionary_reader import read_dictionary_file
 from utilities.word_picker import get_daly_word_index
+from datetime import date
 
 word = APIRouter()
 
@@ -28,6 +29,14 @@ async def load_words(db: db_dependency):
         print(f"The '{word}' word is saved")
 
 @word.get("/get-daily-word", status_code=status.HTTP_200_OK)
-async def get_daily_word(db: Session = Depends(get_db)):
+async def get_daily_word(uid: int,db: Session = Depends(get_db)):
+    # Létezik-e a felhasználó
+    user = db.query(models.User).filter(models.User.id == uid).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    # A user mai probálkozásai 
+    user_today_validates = db.query(models.Validate).filter(models.Validate.uid == uid and models.Validate.date == date.today())
+    if len(user_today_validates.all()) <=6 and len(user_today_validates.filter(models.Validate.result=="1;1;1;1;1").all()) == 0 :
+       raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User must attempt or solve the puzzle six times to access the daily word.")
     words = db.query(models.Word).all()
     return words[get_daly_word_index(len(words))].content
